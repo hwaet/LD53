9529813b3c0c6b537ad7baff1c24a69cd544ae38
Erik Harpstead <eharpste@cs.cmu.edu>
2023-04-30 20:22:11 -0400

    Added a Level 1

--- Assets/Scripts/LevelSpecification.cs
+++ Assets/Scripts/LevelSpecification.cs
@@ -5,9 +5,29 @@ using UnityEngine;
 [CreateAssetMenu]
 public class LevelSpecification : ScriptableObject
 {
+    [TextArea(2, 5)]
     public string narrativeText;
-
+    [TextArea(1, 3)]
     public string targetBlazon;
-
+    [TextArea(1, 3)]
     public List<string> distractorBlazons;
+
+
+    [ContextMenu("Validate Blazons")]
+    public void ValidateBlazons() {
+        try {
+            Blazon test = Blazon.Parse(targetBlazon);
+        }
+        catch(BlazonError e) {
+            Debug.LogErrorFormat("targetBlazon Error: {0}",e.Message);
+        }
+        for (int i = 0; i < distractorBlazons.Count; i++) { 
+            try {
+                Blazon test = Blazon.Parse(distractorBlazons[i]);
+            }
+            catch (BlazonError e) {
+                Debug.LogErrorFormat("distractorBlazon[{0}] Error: {1}", i, e.Message);
+            }
+        }
+    }
 }
