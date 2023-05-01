e13a832f6266cffacdd5f82a90ccd254c3008f81
Hwaet <dev@hwaetgames.com>
2023-04-30 21:37:35 -0700

    Added spite package, and level01 changes

---
+++ Assets/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<LevelSpecification> levels;
+
+    public int currentLevel = 0;
+
+    public Canvas GUI_Dialog;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
